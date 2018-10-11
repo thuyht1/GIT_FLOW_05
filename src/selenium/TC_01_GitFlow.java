7ff9fb9f0dd4bdcf8274ab7eb66c19dfa4e0f36c
thuyht1 <kimthuy.ht106@gmail.com>
2018-10-11 16:46:53 +0700

    LAPTOP_A First commit

---
+++ src/selenium/TC_01_GitFlow.java
@@ -0,0 +1,19 @@
+package selenium;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class TC_01_GitFlow {
+  @Test
+  public void TC_01() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
